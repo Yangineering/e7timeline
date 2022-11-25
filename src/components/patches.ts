09c1913abb6cf1b03e5f0ccaa4e9c441f0600108
Daniel <endqwerty@gmail.com>
2022-11-25 03:12:46 -0800

    3 tables (#1)

---
+++ src/components/patches.ts
@@ -0,0 +1,5 @@
+import { reactive, watch, html } from "@arrow-js/core";
+
+export default function patches() {
+  return html`<iframe class="airtable-embed" src="https://airtable.com/embed/shrM55W6lS1hCKsI0?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>`;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import { reactive, watch, html } from "@arrow-js/core";
-
-
+import events from './components/events';
+import banners from './components/banners';
+import patches from './components/patches';
 const appElement = document.getElementById("app");
 
-const airtableEmbed = html`<iframe
-  class="airtable-embed"
-  src="https://airtable.com/embed/shryxvWCTUs9vPzrh?backgroundColor=green&viewControls=on"
-  frameborder="0"
-  onmousewheel=""
-  width="100%"
-  height="533"
-  style="background: transparent; border: 1px solid #ccc;"
-></iframe>`;
-airtableEmbed(appElement);
+const data = reactive({
+  cardView: false,
+})
+const state = reactive({
+  isOpen: false
+})
+const viewTypeStates = [
+  "Table View",
+  "Card View"
+]
+const app = html`
+<h1>e7 Timeline</h1>
+<h2>Events</h2>
+${events()}
+
+<h2>Banners</h2>
+${banners()}
+
+<h2>Patch Notes</h2>
+${patches()}
+`
+app(appElement);
