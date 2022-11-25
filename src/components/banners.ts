58bf786d9090fb5dbdf4eaad0c8ad0633e4abebe
Daniel Yang <endqwerty@gmail.com>
2022-11-25 03:40:32 -0800

    use cards

--- src/components/banners.ts
+++ src/components/banners.ts
@@ -1,13 +1,5 @@
 import { reactive, watch, html } from "@arrow-js/core";
 
 export default function banners() {
-  return html`<iframe
-    class="airtable-embed"
-    src="https://airtable.com/embed/shryxvWCTUs9vPzrh?backgroundColor=green&viewControls=on"
-    frameborder="0"
-    onmousewheel=""
-    width="100%"
-    height="533"
-    style="background: transparent; border: 1px solid #ccc;"
-  ></iframe>`;
+  return html`<iframe class="airtable-embed" src="https://airtable.com/embed/shryxvWCTUs9vPzrh?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>`;
 }
