bcccbb0a8d1b01496f1d498466be47e6986dc468
Daniel Yang <endqwerty@gmail.com>
2022-11-25 21:10:23 -0800

    use emojis

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -3,12 +3,12 @@
 npm install --global vercel
 if [ $1 = 'main' ]; then
    echo 'On main. Deploying to production'
-   vercel pull --yes --environment=production --token=$VERCEL_TOKEN
-   vercel build --prod --token=$VERCEL_TOKEN
-   vercel deploy --prebuilt --prod  --token=$VERCEL_TOKEN
+   vercel pull --yes --environment=production --token=$VERCEL_AUTH_TOKEN
+   vercel build --prod --token=$VERCEL_AUTH_TOKEN
+   vercel deploy --prebuilt --prod  --token=$VERCEL_AUTH_TOKEN
 else
    echo 'Not on main. Deploying to development'
-   vercel pull --yes --environment=preview --token=$VERCEL_TOKEN
-   vercel build --token=$VERCEL_TOKEN
-   vercel deploy --prebuilt  --token=$VERCEL_TOKEN
+   vercel pull --yes --environment=preview --token=$VERCEL_AUTH_TOKEN
+   vercel build --token=$VERCEL_AUTH_TOKEN
+   vercel deploy --prebuilt  --token=$VERCEL_AUTH_TOKEN
 fi
