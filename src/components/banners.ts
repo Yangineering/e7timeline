a804166d9477c7597f1ca1ec1eb63b7a33a40571
Daniel Yang <endqwerty@gmail.com>
2022-11-26 15:33:59 -0800

    test

--- src/components/banners.ts
+++ src/components/banners.ts
@@ -2,9 +2,9 @@ import { reactive, watch, html } from "@arrow-js/core";
 
 export default function banners() {
   return html`
-  <div class="col-span-2">  
+  <div class="ol-span-1 2xl:col-span-2">  
     <h2>Banners</h2>
-    <iframe class="airtable-embed w-full h-full" src="https://airtable.com/embed/shryxvWCTUs9vPzrh?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
+    <iframe class="airtable-embed w-full h-600" src="https://airtable.com/embed/shryxvWCTUs9vPzrh?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
   </div>
   `;
 }
