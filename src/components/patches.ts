214dcdcf7deae589d809c75bb72dc39b9f7bdbe8
Daniel Yang <endqwerty@gmail.com>
2022-11-26 15:05:47 -0800

    make larger

--- src/components/patches.ts
+++ src/components/patches.ts
@@ -4,7 +4,7 @@ export default function patches() {
   return html`
   <div class="col-span-2 h-100">  
     <h2>Patches</h2>
-    <iframe class="airtable-embed w-full" src="https://airtable.com/embed/shrM55W6lS1hCKsI0?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
+    <iframe class="airtable-embed w-full h-full" src="https://airtable.com/embed/shrM55W6lS1hCKsI0?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
   </div>
   `;
 }
