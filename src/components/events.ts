06b811232b748b8a92c20bb6a9e5451d4dde4681
Daniel Yang <endqwerty@gmail.com>
2022-11-26 14:34:45 -0800

    make events 1

--- src/components/events.ts
+++ src/components/events.ts
@@ -2,7 +2,7 @@ import { reactive, watch, html } from "@arrow-js/core";
 
 export default function events() {
   return html`
-  <div class="cols-span-2">  
+  <div class="cols-span-1">  
     <h2>Events</h2>
     <iframe class="airtable-embed" src="https://airtable.com/embed/shrnhVMltxFsLEYWu?backgroundColor=green&layout=card" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
   </div>
