97cac418bdaa3467c688811886070bff4ac8ec16
Daniel Yang <endqwerty@gmail.com>
2022-11-26 15:53:49 -0800

    update heights

--- src/components/patches.ts
+++ src/components/patches.ts
@@ -2,7 +2,7 @@ import { reactive, watch, html } from "@arrow-js/core";
 
 export default function patches() {
   return html`
-  <div class="col-span-1 2xl:col-span-2 h-40 flex flex-col">  
+  <div class="col-span-1 2xl:col-span-2 h-96 flex flex-col">  
     <h2>Patches</h2>
     <iframe class="airtable-embed flex-1" src="https://airtable.com/embed/shrM55W6lS1hCKsI0?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
   </div>
