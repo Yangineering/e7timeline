73e3c63b2f96e88bacd1c9c168fa0bf2e1df6f5d
Daniel Yang <endqwerty@gmail.com>
2022-11-26 14:28:43 -0800

    test flex basis

--- src/components/events.ts
+++ src/components/events.ts
@@ -1,5 +1,5 @@
 import { reactive, watch, html } from "@arrow-js/core";
 
 export default function events() {
-  return html`<iframe class="airtable-embed" src="https://airtable.com/embed/shrnhVMltxFsLEYWu?backgroundColor=green&layout=card" frameborder="0" onmousewheel="" width="10%" min-width="100" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>`;
+  return html`<iframe class="airtable-embed md:basis-2" src="https://airtable.com/embed/shrnhVMltxFsLEYWu?backgroundColor=green&layout=card" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>`;
 }
