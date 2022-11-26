a7d338fad7a4474ce15bc5054dc652295569d317
Daniel Yang <endqwerty@gmail.com>
2022-11-25 20:20:58 -0800

    update

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -1,9 +1,11 @@
  #!/usr/bin/bash 
 
- if [ $1 = 'main' ]; then
-    echo 'On main. Deploying to production'
-    npx vercel --prod --token=$VERCEL_AUTH_TOKEN
- else
-    echo 'Not on main. Deploying to development'
-    npx vercel --token=$VERCEL_AUTH_TOKEN
- fi
+echo $$VERCEL_AUTH_TOKEN
+
+if [ $1 = 'main' ]; then
+   echo 'On main. Deploying to production'
+   npx vercel --prod --token=$VERCEL_AUTH_TOKEN
+else
+   echo 'Not on main. Deploying to development'
+   npx vercel --token=$VERCEL_AUTH_TOKEN
+fi
