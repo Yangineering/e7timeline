f604d0750ff3a6c9b1bc7b337af8c191975accc8
Daniel Yang <endqwerty@gmail.com>
2022-11-26 15:50:13 -0800

    use normal numbers

--- src/components/banners.ts
+++ src/components/banners.ts
@@ -2,7 +2,7 @@ import { reactive, watch, html } from "@arrow-js/core";
 
 export default function banners() {
   return html`
-  <div class="ol-span-1 2xl:col-span-2 h-400 flex flex-col">  
+  <div class="col-span-1 2xl:col-span-2 h-40 flex flex-col">  
     <h2>Banners</h2>
     <iframe class="airtable-embed flex-1" src="https://airtable.com/embed/shryxvWCTUs9vPzrh?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
   </div>
--- src/components/events.ts
+++ src/components/events.ts
@@ -2,7 +2,7 @@ import { reactive, watch, html } from "@arrow-js/core";
 
 export default function events() {
   return html`
-  <div class="col-span-1 flex h-600  flex-col">  
+  <div class="col-span-1 h-96 flex flex-col">  
     <h2>Events</h2>
     <iframe class="airtable-embed flex-1" src="https://airtable.com/embed/shrnhVMltxFsLEYWu?backgroundColor=green&layout=card" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
   </div>
