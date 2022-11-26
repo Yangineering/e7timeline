4d242229aec3c1d332f7e6457fc4bbc2bc2ba060
Daniel Yang <endqwerty@gmail.com>
2022-11-26 15:02:16 -0800

    use full width

--- src/components/banners.ts
+++ src/components/banners.ts
@@ -2,9 +2,9 @@ import { reactive, watch, html } from "@arrow-js/core";
 
 export default function banners() {
   return html`
-  <div>  
+  <div class="col-span-2 h-50">  
     <h2>Banners</h2>
-    <iframe class="airtable-embed col-span-2 h-50" src="https://airtable.com/embed/shryxvWCTUs9vPzrh?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
+    <iframe class="airtable-embed w-full" src="https://airtable.com/embed/shryxvWCTUs9vPzrh?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
   </div>
   `;
 }
--- src/components/patches.ts
+++ src/components/patches.ts
@@ -2,9 +2,9 @@ import { reactive, watch, html } from "@arrow-js/core";
 
 export default function patches() {
   return html`
-  <div>  
+  <div class="col-span-2 h-100">  
     <h2>Patches</h2>
-    <iframe class="airtable-embed col-span-2 h-50" src="https://airtable.com/embed/shrM55W6lS1hCKsI0?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
+    <iframe class="airtable-embed w-full" src="https://airtable.com/embed/shrM55W6lS1hCKsI0?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
   </div>
   `;
 }
