ec6a4e492c4100e44cdedfaeb70e136e1719baf2
Daniel Yang <endqwerty@gmail.com>
2022-11-26 14:16:22 -0800

    test

--- src/components/events.ts
+++ src/components/events.ts
@@ -1,5 +1,5 @@
 import { reactive, watch, html } from "@arrow-js/core";
 
 export default function events() {
-  return html`<iframe class="airtable-embed" src="https://airtable.com/embed/shrnhVMltxFsLEYWu?backgroundColor=green&viewControls=on&layout=card" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>`;
+  return html`<iframe class="airtable-embed" src="https://airtable.com/embed/shrnhVMltxFsLEYWu?backgroundColor=green&layout=card" frameborder="0" onmousewheel="" width="10%" min-width="100" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>`;
 }
