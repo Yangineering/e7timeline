277fb1dbd833bc2acb58a8f60304ebb06ebf8ef8
Daniel Yang <endqwerty@gmail.com>
2022-11-26 15:14:30 -0800

    set height

--- src/components/events.ts
+++ src/components/events.ts
@@ -2,7 +2,7 @@ import { reactive, watch, html } from "@arrow-js/core";
 
 export default function events() {
   return html`
-  <div class="col-span-2 2xl:col-span-1 h-200">  
+  <div class="col-span-2 2xl:col-span-1">  
     <h2>Events</h2>
     <iframe class="airtable-embed w-full h-full" src="https://airtable.com/embed/shrnhVMltxFsLEYWu?backgroundColor=green&layout=card" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
   </div>
