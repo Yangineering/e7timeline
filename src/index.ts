8c8d13fa32d4ecd70726796787b3b1e252a15bf3
Daniel Yang <endqwerty@gmail.com>
2022-11-25 17:16:59 -0800

    add buildkite

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
-import { reactive, watch, html } from "@arrow-js/core";
+import { r, w, t } from "@arrow-js/core";
 import events from './components/events';
 import banners from './components/banners';
 import patches from './components/patches';
 const appElement = document.getElementById("app");
 
-const data = reactive({
-  cardView: false,
+const state = r({
+  tabs: [
+    { name: 'Events' },
+    { name: 'Banners' },
+    { name: 'Patches' }
+  ]
 })
-const state = reactive({
-  isOpen: false
-})
-const viewTypeStates = [
-  "Table View",
-  "Card View"
-]
-const app = html`
+
+const app = t`
 <h1>e7 Timeline</h1>
 <h2>Events</h2>
 ${events()}
