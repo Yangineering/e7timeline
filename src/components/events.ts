912c1d2e6a0aa613b4b9d0083431f1743d2ad4f5
Daniel Yang <endqwerty@gmail.com>
2022-11-26 15:43:32 -0800

    use flex

--- src/components/events.ts
+++ src/components/events.ts
@@ -2,9 +2,9 @@ import { reactive, watch, html } from "@arrow-js/core";
 
 export default function events() {
   return html`
-  <div class="col-span-1">  
+  <div class="col-span-1 flex h-600  flex-col">  
     <h2>Events</h2>
-    <iframe class="airtable-embed w-full h-400" src="https://airtable.com/embed/shrnhVMltxFsLEYWu?backgroundColor=green&layout=card" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
+    <iframe class="airtable-embed flex-1" src="https://airtable.com/embed/shrnhVMltxFsLEYWu?backgroundColor=green&layout=card" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
   </div>
   `;
 }
