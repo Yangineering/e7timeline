21fde7bc670e0859d5fbb7fbb60a9181dd1c295b
Daniel Yang <endqwerty@gmail.com>
2022-11-26 16:51:56 -0800

    add about page

---
+++ src/components/about.ts
@@ -0,0 +1,8 @@
+import { reactive, watch, html } from "@arrow-js/core";
+
+export default function about() {
+return html`
+<h2>About</h2>
+Contact: e7timeline@endqwerty.com
+`
+}
--- src/components/banners.ts
+++ src/components/banners.ts
@@ -2,7 +2,7 @@ import { reactive, watch, html } from "@arrow-js/core";
 
 export default function banners() {
   return html`
-  <div class="col-span-1 2xl:col-span-2 h-96 flex flex-col">  
+  <div class="h-120 flex flex-col">  
     <h2>Banners</h2>
     <iframe class="airtable-embed flex-1" src="https://airtable.com/embed/shryxvWCTUs9vPzrh?backgroundColor=green&viewControls=on" frameborder="0" onmousewheel="" style="background: transparent; border: 1px solid #ccc;"></iframe>
   </div>
