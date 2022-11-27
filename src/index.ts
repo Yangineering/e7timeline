8adecc4e9b9ed532df9676510b860455f1669391
Daniel Yang <endqwerty@gmail.com>
2022-11-26 16:09:35 -0800

    use navbar

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,35 @@ const appElement = document.getElementById("app");
 
 const state = r({
   tabs: [
-    { name: 'Events' },
-    { name: 'Banners' },
-    { name: 'Patches' }
-  ]
+    { name: 'Events', element: events() },
+    { name: 'Banners', element: banners() },
+    { name: 'Patches', element: patches() }
+  ],
+  selectedTab: 0
 })
 
 const app = t`
 <h1>e7 Timeline</h1>
-<div class="grid grid-cols-1 2xl:grid-cols-5 w-full">
-  ${events()}
 
-  ${banners()}
-
-  ${patches()}
+<div class="container">
+  <div class="tabs">
+    ${() => state.tabs.map((tab, index) =>
+      t`
+        <button
+          class="${() => index === state.selectedTab ? 'underline' : ''}"
+          @click="${() => (state.selectedTab = index)}"
+        >
+          <p>${tab.name}</p>
+        </button>
+      `
+    )}
+    <div
+      class="tab-indicator"
+    ></div>
+  </div>
+  <div class="tab-panels">
+    ${() => state.tabs[state.selectedTab].element}
+  </div>
 </div>
 `
 app(appElement);
